b5a2ce663b7212a1562a6c3a09cae9cb0c9f26db
yepez1990 <pyepez@ari.group>
2023-02-20 18:08:27 -0600

    avanze 1 js

---
+++ js/introjs.js
@@ -0,0 +1,7 @@
+//alert("hola mundo");
+// console.log("hola, saludos a la consola")
+// console.log(2+2);
+// console.info("montrando info");
+// console.error("mostrando error");
+// console.warn("mostrando warm");
+document.write("hola js");
